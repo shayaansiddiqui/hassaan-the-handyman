929dc8cd783523f7ab6e181e71f00d3afb607e65
Shayaan Siddiqui <shayaan.siddiqui@gmail.com>
2024-04-01 10:25:29 -0400

    updated menu

--- src/Shared/Header/Menu.tsx
+++ src/Shared/Header/Menu.tsx
@@ -3,20 +3,25 @@ import {MenuItem} from "../../Interface/MenuItem.tsx";
 import MenuItemElement from "./MenuItemElement.tsx";
 import styles from './Menu.module.css';
 
-const Menu = ({ items }: { items: MenuItem[] }) => (
-    <ul className={styles.menuList}>
-        {items?.sort((a, b) => a.label.localeCompare(b.label))?.map((item) => (
-        <li key={item.label}>
-            <MenuItemElement label={item.label} url={item.url} />
-            {item.children && (
-                <div className={styles.submenu}>
-                    <Menu items={item.children} />
-                </div>
-            )}
-        </li>
-        ))
-    }
-    </ul>
-);
+const Menu = ({ items }: { items: MenuItem[] }) => {
+    //const sortedItems = items.sort((a, b) => a.label.localeCompare(b.label));
+
+
+    return (
+        <ul className={styles.menuList}>
+            {items.map((item) => (
+                <li key={item.label}>
+                    <MenuItemElement label={item.label} url={item.url}/>
+                    {item.children && (
+                        <div className={styles.submenu}>
+                            <Menu
+                                items={item.children ? item.children.sort((a, b) => a.label.localeCompare(b.label)) : item.children}/>
+                        </div>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default Menu;
